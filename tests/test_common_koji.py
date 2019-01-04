f21a003775defe5e616496169b22a4e9ed0781c7
Ken Dreyer <kdreyer@redhat.com>
2019-01-03 19:01:33 -0700

    add tests for common_koji library

---
+++ tests/test_common_koji.py
@@ -0,0 +1,35 @@
+# from common_koji import get_profile_name, get_session, ensure_logged_in
+from common_koji import get_profile_name
+from ansible.errors import AnsibleError
+import pytest
+
+
+def test_get_profile_name():
+    assert get_profile_name('fakekoji') == 'fakekoji'
+
+
+def test_get_profile_name_from_env(monkeypatch):
+    monkeypatch.setenv('KOJI_PROFILE', 'fakekoji')
+    assert get_profile_name(None) == 'fakekoji'
+
+
+def test_get_profile_name_error():
+    with pytest.raises(AnsibleError) as e:
+        get_profile_name(None)
+    assert 'KOJI_PROFILE environment variable' in str(e)
+
+
+"""
+Live tests, need to figure out how to mock these out:
+
+def test_get_session():
+    profile = 'kojidev'
+    session = get_session(profile)
+    assert session.logged_in is False
+
+def test_ensure_logged_in():
+    profile = 'kojidev'
+    session = get_session(profile)
+    ensure_logged_in(session)
+    assert session.logged_in is True
+"""
