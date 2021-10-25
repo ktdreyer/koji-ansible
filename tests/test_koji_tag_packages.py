5b39f75ef3a0a722839a7280bdb56faa59fc94a1
Ken Dreyer <kdreyer@redhat.com>
2021-10-25 11:52:35 -0400

    tag_packages: real package names and tag name in tests
    Use real packages and a real tag name ("epel8") to make the tests easier
    to understand.

--- tests/test_koji_tag_packages.py
+++ tests/test_koji_tag_packages.py
@@ -7,123 +7,123 @@ class TestKojiTagPackages(object):
 
     def test_remove_packages(self):
         packages = {
-            "user1": ['foo', 'bar'],
-            "user2": ['baz'],
+            "user1": ['ceph', 'curl'],
+            "user2": ['coreutils'],
         }
         check_mode = False
         session = Mock()
         result = koji_tag_packages.remove_packages(
-            session, "tag", check_mode, packages)
+            session, "epel8", check_mode, packages)
         assert result['changed']
         calls = [
-            call("tag", "foo", "user1"),
-            call("tag", "bar", "user1"),
-            call("tag", "baz", "user2"),
+            call("epel8", "ceph", "user1"),
+            call("epel8", "curl", "user1"),
+            call("epel8", "coreutils", "user2"),
         ]
         session.packageListRemove.assert_has_calls(calls, any_order=True)
 
     def test_add_package(self):
         packages = {
-            "user1": ['foo', 'bar'],
-            "user2": ['baz'],
+            "user1": ['ceph', 'curl'],
+            "user2": ['coreutils'],
         }
         current_packages = [
-            {"package_name": "foo", "owner_name": "user1"},
-            {"package_name": "baz", "owner_name": "user2"},
+            {"package_name": "curl", "owner_name": "user1"},
+            {"package_name": "coreutils", "owner_name": "user2"},
         ]
         check_mode = False
         session = Mock()
         session.listPackages = Mock(return_value=current_packages)
         result = koji_tag_packages.ensure_packages(
-            session, "tag", "5", check_mode, packages)
+            session, "epel8", "5", check_mode, packages)
         assert result['changed']
-        session.packageListAdd.assert_called_with("tag", "bar", "user1")
+        session.packageListAdd.assert_called_with("epel8", "ceph", "user1")
 
     def test_packages_no_change(self):
         packages = {
-            "user1": ['foo', 'bar'],
-            "user2": ['baz'],
+            "user1": ['ceph', 'curl'],
+            "user2": ['coreutils'],
         }
         current_packages = [
-            {"package_name": "foo", "owner_name": "user1"},
-            {"package_name": "bar", "owner_name": "user1"},
-            {"package_name": "baz", "owner_name": "user2"},
+            {"package_name": "ceph", "owner_name": "user1"},
+            {"package_name": "curl", "owner_name": "user1"},
+            {"package_name": "coreutils", "owner_name": "user2"},
         ]
         check_mode = False
         session = Mock()
         session.listPackages = Mock(return_value=current_packages)
         result = koji_tag_packages.ensure_packages(
-            session, "tag", "5", check_mode, packages)
+            session, "epel8", "5", check_mode, packages)
         assert not result['changed']
 
     def test_unblock_packages(self):
         packages = [
-            'foo',
-            'bar',
+            'ceph',
+            'curl',
         ]
         current_packages = [
-            {"package_name": "foo", "blocked": True},
-            {"package_name": "bar", "blocked": True},
+            {"package_name": "ceph", "blocked": True},
+            {"package_name": "curl", "blocked": True},
         ]
         check_mode = False
         session = Mock()
         session.listPackages = Mock(return_value=current_packages)
         result = koji_tag_packages.remove_package_blocks(
-            session, "tag", check_mode, packages)
+            session, "epel8", check_mode, packages)
         assert result == [
-            'unblock pkg foo',
-            'unblock pkg bar',
+            'unblock pkg ceph',
+            'unblock pkg curl',
         ]
         calls = [
-            call("tag", "foo"),
-            call("tag", "bar"),
+            call("epel8", "ceph"),
+            call("epel8", "curl"),
         ]
         session.packageListUnblock.assert_has_calls(calls, any_order=True)
 
     def test_block_packages(self):
-        packages = ["foo", "bar", "baz"]
+        packages = ["ceph", "curl", "coreutils"]
         current_packages = [
-            {"package_name": "foo", "blocked": True},
-            {"package_name": "baz", "blocked": True},
+            {"package_name": "ceph", "blocked": True},
+            {"package_name": "coreutils", "blocked": True},
         ]
 
         check_mode = False
         session = Mock()
         session.listPackages = Mock(return_value=current_packages)
         result = koji_tag_packages.ensure_blocked_packages(
-            session, "tag", "5", check_mode, packages)
-        assert result == ['block pkg bar']
-        session.packageListBlock.assert_called_with("tag", "bar")
+            session, "epel8", "5", check_mode, packages)
+        assert result == ['block pkg curl']
+        session.packageListBlock.assert_called_with("epel8", "curl")
 
     def test_block_no_change(self):
-        packages = ["foo", "bar", "baz"]
+        packages = ["ceph", "curl", "coreutils"]
         current_packages = [
-            {"package_name": "foo", "blocked": True},
-            {"package_name": "baz", "blocked": True},
-            {"package_name": "bar", "blocked": True},
+            {"package_name": "ceph", "blocked": True},
+            {"package_name": "coreutils", "blocked": True},
+            {"package_name": "curl", "blocked": True},
         ]
 
         check_mode = False
         session = Mock()
         session.listPackages = Mock(return_value=current_packages)
         result = koji_tag_packages.ensure_blocked_packages(
-            session, "tag", "5", check_mode, packages)
+            session, "epel8", "5", check_mode, packages)
         assert result == []
 
     def test_fix_package_ownership(self):
         packages = {
-            "user1": ['foo', 'bar'],
-            "user2": ['baz'],
+            "user1": ['ceph', 'curl'],
+            "user2": ['coreutils'],
         }
         current_packages = [
-            {"package_name": "foo", "owner_name": "user1"},
-            {"package_name": "bar", "owner_name": "user1"},
-            {"package_name": "baz", "owner_name": "user1"},
+            {"package_name": "ceph", "owner_name": "user1"},
+            {"package_name": "curl", "owner_name": "user1"},
+            {"package_name": "coreutils", "owner_name": "user1"},
         ]
         check_mode = False
         session = Mock()
         session.listPackages = Mock(return_value=current_packages)
         result = koji_tag_packages.ensure_packages(
-            session, "tag", "5", check_mode, packages)
+            session, "epel8", "5", check_mode, packages)
         assert result['changed']
-        session.packageListSetOwner.assert_called_with("tag", "baz", "user2")
+        session.packageListSetOwner.assert_called_with("epel8", "coreutils", "user2")
